4a848aa1cc95f2ba9b4e8984a6b8e13990115c0a
Galih Anggoro Jati <galih.anggoro@kopnus.com>
2025-08-10 21:31:58 +0700

    feat: add log hook telegram

--- config/logrus.go
+++ config/logrus.go
@@ -1,14 +1,85 @@
 package config
 
 import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/sirupsen/logrus"
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
+// Telegram Hooks
+type TelegramHook struct {
+	Token  string
+	ChatID string
+}
+
+// level2 yang dikirim hook
+func (hook *TelegramHook) Levels() []logrus.Level {
+	return []logrus.Level{
+		logrus.ErrorLevel,
+		logrus.FatalLevel,
+		logrus.PanicLevel,
+	}
+}
+
+// fire dieksekusi saat log dengan levels di method Levels dipanggil
+func (hook *TelegramHook) Fire(entry *logrus.Entry) error {
+	// format pesan notifnya
+	msg := fmt.Sprintf("App: %s\nLevel: %s\nMessage: %s\nTime: %s\n",
+		os.Getenv("APP_NAME"),
+		entry.Level,
+		entry.Message,
+		entry.Time.Format("2006-01-02 15:04:05"),
+	)
+
+	var msgBuilder strings.Builder
+	msgBuilder.WriteString(msg)
+
+	// tambahkan semua fields dari entry.Data
+	if len(entry.Data) > 0 {
+		msgBuilder.WriteString("*Fields*:\n")
+		for key, value := range entry.Data {
+			msgBuilder.WriteString(fmt.Sprintf(" - %s: %v\n", key, value))
+		}
+	}
+
+	// URL API Telegram untuk send messsage
+	apiUrl := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", hook.Token)
+
+	// data form untuk POST request
+	data := url.Values{}
+	data.Set("chat_id", hook.ChatID)
+	data.Set("text", msgBuilder.String())
+	data.Set("parse_mode", "Markdown") // Optional: untuk format teks
+
+	// kirim http post
+	resp, err := http.PostForm(apiUrl, data)
+
+	if err != nil {
+		return fmt.Errorf("gagal kirim notifikasi ke telegram: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return fmt.Errorf("gagal membaca response body telegram: %s", err.Error())
+		}
+		return fmt.Errorf("respons telegram tidak OK: status: %d, response: %s", resp.StatusCode, string(bodyBytes))
+	}
+
+	return nil
+}
+
+// Initialize logger
 func NewLogger() *logrus.Logger {
 	logger := logrus.New()
 	logger.SetFormatter(&logrus.JSONFormatter{})
@@ -33,6 +104,18 @@ func NewLogger() *logrus.Logger {
 
 	// Mengatur level log
 	logger.SetLevel(logrus.InfoLevel)
+
+	logHook := os.Getenv("LOG_HOOK")
+
+	// add hook telegram
+	if logHook == "telegram" {
+		hook := &TelegramHook{
+			Token:  os.Getenv("TELEGRAM_TOKEN"),
+			ChatID: os.Getenv("TELEGRAM_CHAT_ID"),
+		}
+
+		logger.AddHook(hook)
+	}
 
 	return logger
 }
--- go.mod
+++ go.mod
@@ -7,7 +7,9 @@ require (
 	github.com/gofiber/fiber/v2 v2.52.9
 	github.com/golang-jwt/jwt/v5 v5.2.3
 	github.com/joho/godotenv v1.5.1
+	github.com/sirupsen/logrus v1.9.3
 	golang.org/x/crypto v0.40.0
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.30.1
 )
@@ -30,7 +32,6 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
@@ -38,5 +39,4 @@ require (
 	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/sys v0.34.0 // indirect
 	golang.org/x/text v0.27.0 // indirect
-	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 )
